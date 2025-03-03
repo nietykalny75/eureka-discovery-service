04ec1412c8c00d6c7464cc884ae396b29ef6baaf
nietykalny75 <tomasz.sowinski@nucleus.malbork.pl>
2025-03-03 16:59:32 +0100

    Rename files and moved configuration to config server

--- src/main/java/pl/malbork/nucleus/EurekaDiscoveryService.java
+++ src/main/java/pl/malbork/nucleus/EurekaDiscoveryService.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaDiscoveryService {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(EurekaDiscoveryService.class, args);
     }
 }
